f486df20b89b2fff3a6674691ad71c2f9a441c5d
ianSama0217 <ian20000217@gmail.com>
2023-08-17 18:36:05 +0800

    add new post function

--- routes/profile-router.js
+++ routes/profile-router.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Post = require("../models/post-model");
 
 const checkAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -8,9 +9,26 @@ const checkAuth = (req, res, next) => {
   }
 };
 
-router.get("/", checkAuth, (req, res) => {
+router.get("/", checkAuth, async (req, res) => {
   console.log("進入profile..");
-  return res.render("profile", { user: req.user });
+  let foundPost = await Post.find({ author: req.user._id });
+  return res.render("profile", { user: req.user, posts: foundPost });
+});
+
+router.get("/post", checkAuth, (req, res) => {
+  return res.render("post", { user: req.user });
+});
+
+router.post("/post", checkAuth, async (req, res) => {
+  let { title, content } = req.body;
+  let newPost = new Post({ title, content, author: req.user._id });
+  try {
+    await newPost.save();
+    return res.redirect("/profile");
+  } catch (e) {
+    req.flash("error_msg", "標題與內容都需要填寫");
+    return res.redirect("/profile/post");
+  }
 });
 
 module.exports = router;
